cea882bae2c1cf2d36799af596d260f908c17540
Guilherme Lopes Roque <guilherme.roque@intelbras.com.br>
2021-11-27 19:48:58 -0300

    Second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const moment = require('moment')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log("Hello WORLD!!AAAAAAAAAA")
+  console.log("Hello WORLD!!")
   res.render('index', { title: 'Express' });
 });
 
--- routes/schedule.js
+++ routes/schedule.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const schedule = require("../models/scheduleDAO")
+const scheduleDAO = require("../models/scheduleDAO")
+const moment = require("moment")
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond with a resource');
@@ -8,8 +10,12 @@ router.get('/', function(req, res, next) {
 
 router.post('/',(req,res)=>{
     console.log(req.body)
-    schedule.add(req.body)
-    res.send("OK")
+    const date = moment(new Date()).format('YYYY-MM-DD HH:MM:SS')
+    console.log(date)
+    const schedule = {...req.body,date}
+    console.log(schedule)
+    scheduleDAO.add(schedule)
+    res.send(schedule)
 })
 
 module.exports = router;
